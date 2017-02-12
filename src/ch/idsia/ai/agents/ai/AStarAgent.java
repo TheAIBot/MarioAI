d205ada8f78561058251b417e88cca2ed2135150
FantAsterix <fantasterix@gmail.com>
2017-02-12 15:11:35 +0100

    Reformat AStarAgent

--- src/ch/idsia/ai/agents/ai/AStarAgent.java
+++ src/ch/idsia/ai/agents/ai/AStarAgent.java
@@ -6,26 +6,21 @@ import ch.idsia.mario.environments.Environment;
 
 public class AStarAgent extends BasicAIAgent implements Agent {
 
-    static final boolean superslow = false;
+	static final boolean superslow = false;
 
-    public AStarAgent()
-    {
-        super("AStarAgent");
-        reset();
-    }
+	public AStarAgent() {
+		super("AStarAgent");
+		reset();
+	}
 
-    public void reset()
-    {
-        action = new boolean[Environment.numberOfButtons];
-        action[Mario.KEY_RIGHT] = true;
-        action[Mario.KEY_SPEED] = true;
-    }
+	public void reset() {
+		action = new boolean[Environment.numberOfButtons];
+		action[Mario.KEY_RIGHT] = true;
+		action[Mario.KEY_SPEED] = true;
+	}
 
-    public boolean[] getAction(Environment observation)
-    {
-//        try {Thread.sleep (39);}
-//        catch (Exception e){}
-        action[Mario.KEY_SPEED] = action[Mario.KEY_JUMP] =  observation.mayMarioJump() || !observation.isMarioOnGround();
-        return action;
-    }
+	public boolean[] getAction(Environment observation) {
+		action[Mario.KEY_SPEED] = action[Mario.KEY_JUMP] = observation.mayMarioJump() || !observation.isMarioOnGround();
+		return action;
+	}
 }
