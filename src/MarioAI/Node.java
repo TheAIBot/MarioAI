960c07cc55135f21d48d0f6be2b95b7193cf3033
TheAIBot <andreasuranus@hotmail.com>
2017-02-12 15:54:02 +0100

    added hasher and equals to node

--- src/MarioAI/Node.java
+++ src/MarioAI/Node.java
@@ -6,10 +6,22 @@ public class Node {
 	public ArrayList<Node> edges = new ArrayList<Node>();
 	public final short x;
 	public final short y;
-	
-	public Node(short x, short y)
-	{
+
+	public Node(short x, short y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	public boolean Equals(Object b) {
+		if (b == null) {
+			return false;
+		}
+		if (b instanceof Node) {
+			Node bb = (Node) b;
+			return Hasher.hashShortPoint(bb.x, bb.y) == Hasher.hashShortPoint(
+					x, y);
+		} else {
+			return false;
+		}
 	}
 }
---
+++ src/MarioAI/surface.java
@@ -0,0 +1,5 @@
+package MarioAI;
+
+public class surface {
+
+}
