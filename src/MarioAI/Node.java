9e488f667a68599b8130c14b60ec230a50ba6b32
FantAsterix <fantasterix@gmail.com>
2017-02-13 15:03:29 +0100

    Added SuperNode class
    To handle surfaces and normal nodes in map graphs

--- src/MarioAI/Node.java
+++ src/MarioAI/Node.java
@@ -1,38 +1,15 @@
 package MarioAI;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Node implements Comparable<Node> {
-	public ArrayList<Node> edges = new ArrayList<Node>();
+public class Node extends SuperNode {
 	public final short x;
 	public final short y;
-	public int gScore; // g(n) for current node
-	public int fScore; // f(n) for current node
-	private static int LARGE_NUMBER = 10000; // temp in place of infinity
-	public Node parent; //cameFrom
-	private List<Node> neighbors;
 
 	public Node(short x, short y) {
+		super();
 		this.x = x;
 		this.y = y;
-		gScore = LARGE_NUMBER;
-		gScore = LARGE_NUMBER;
-		parent = null;
-		neighbors = new ArrayList<Node>();
-	}
-	
-	public List<Node> getNeighbors() {
-		return neighbors;
 	}
 
-	@Override
-	public int compareTo(Node o) {
-		if (this.gScore < o.fScore) return -1;
-		if (this.gScore > o.fScore) return 1;
-		return 0;
-	}
-	
 	@Override
 	public boolean equals(Object o) {
 		if (!(o instanceof Node)) return false;
