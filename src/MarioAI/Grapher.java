845e16e86b790f66acdcffed3db1ee9ba3064276
Lombre <jesper.birch3@gmail.com>
2017-02-19 13:34:55 +0100

    Mario Jump polynomials working for lenght 6 polynomials.

--- src/MarioAI/Grapher.java
+++ src/MarioAI/Grapher.java
@@ -4,153 +4,247 @@ import java.util.*;
 
 import ch.idsia.mario.environments.Environment;
 import jdk.internal.dynalink.beans.StaticClass;
+import sun.net.www.content.audio.x_aiff;
 
 public class Grapher {
 	Environment observation;
 	private byte[][] currentObservation;
+	private final short JUMP_HEIGHT = 4;
+	private final short MAX_JUMP_RANGE = 6;
 	private final short GRID_SIZE = 22;
 	private Node[][] observationGraph = new Node[GRID_SIZE][GRID_SIZE];
 	private boolean[][] inRecursion = new boolean[GRID_SIZE][GRID_SIZE];
 	private final short MARIO_JUMP_LENGHT = 5;
 	private Node marioNode;
-	int testPrintCounter = 24; //Rand value
-	
-	
-	
+	int testPrintCounter = 24; // Rand value
+
 	public Grapher(Environment observation) {
 		this.observation = observation;
 		currentObservation = observation.getCompleteObservation();
 	}
-	
+
 	public void updateLevelGraph() {
 		currentObservation = this.observation.getCompleteObservation();
-	}	
-	
+	}
+
 	public void printView() {
 		testPrintCounter = testPrintCounter % 240;
 		if (testPrintCounter == 0) {
+			System.out.printf("    ");
+			for (int j = 0; j < GRID_SIZE; j++) {
+				System.out.printf("%03d ", j);
+			}
+			System.out.println();
+			
 			for (int i = 0; i < GRID_SIZE; i++) {
+				System.out.printf("%03d ", i);				
 				for (int j = 0; j < GRID_SIZE; j++) {
-					if(i == 11 && j==11) {
-						System.out.printf("  M ",currentObservation[i][j]);
-					}else System.out.printf("%03d ",currentObservation[i][j]);
+					if (i == 11 && j == 11) 
+						System.out.printf("MMM ", currentObservation[i][j]);
+					else
+						System.out.printf("%03d ", currentObservation[i][j]);
 				}
 				System.out.println();
 			}			
-			observationGraph = new Node[GRID_SIZE][GRID_SIZE]; //Test			
-			marioNode = new Node((short) 0, (short) 0, (short) (GRID_SIZE/2), (short) (GRID_SIZE/2),  
-					 currentObservation[GRID_SIZE/2][GRID_SIZE/2]);			
-			observationGraph[GRID_SIZE/2][GRID_SIZE/2]= marioNode; //Mario node
-			inRecursion[GRID_SIZE/2][GRID_SIZE/2] = true;
-			if(isSolid(currentObservation[marioNode.row - 1][marioNode.coloumn])) connectNode(marioNode);
-			System.out.println("The observation.");			
+			observationGraph = new Node[GRID_SIZE][GRID_SIZE]; // Test
+			marioNode = new Node((short) 0, (short) 0, (short) (GRID_SIZE / 2), (short) (GRID_SIZE / 2),
+					currentObservation[GRID_SIZE / 2][GRID_SIZE / 2]);
+			observationGraph[GRID_SIZE / 2][GRID_SIZE / 2] = marioNode; // Mario
+																		// node
+			inRecursion[GRID_SIZE / 2][GRID_SIZE / 2] = true;
+				if (isSolid(currentObservation[marioNode.row + 1][marioNode.coloumn]))
+				connectNode(marioNode);
+			System.out.println("The observation.");
 		}
-		testPrintCounter++;	
-		
+		testPrintCounter++;
+
 	}
 	
 	private void connectNode(Node node) {
-		//For efficiency, it is possible to make this into an iterative function, instead of a recursive method, 
-		//either by using a stack (depth first),or an queue (breadth first).
-		
-		//Maybe detect enemy.		
-		//Remember to use mario's speed in calculations.	
-		
-		//Find the reachable nodes:
-		List<Node> circleNodes = getReachableNodes(node);
-		for (int i = 0; i < circleNodes.size(); i++) {
-			if (canMarioStandThere(circleNodes.get(i))) { //FIX
-				node.edges.add(circleNodes.get(i));
-			}
-		}									
-		//Recursion over the reachable nodes:
-		for (Node edge : node.edges) {
-			if (!inRecursion[edge.row][edge.coloumn]) {
-				inRecursion[edge.row][edge.coloumn] = true; //No infinite recursion.
-				connectNode(edge); //Thus it is depth first.
+		// For efficiency, it is possible to make this into an iterative
+		// function, instead of a recursive method,
+		// either by using a stack (depth first),or an queue (breadth first).
+
+		// Maybe detect enemy.
+		// Remember to use mario's speed in calculations.
+
+		// Find the reachable nodes:
+		List<Node> reachableNodes = getReachableNodes(node);
+		for (Node foundNode : reachableNodes) {
+			if (canMarioStandThere(foundNode)) { // FIX
+				node.edges.add(foundNode); //TODO Fix the fact that there are no guarantee that there aren't duplicates.
 			}
 		}
-		
-	
+		// Recursion over the reachable nodes:
+		for (Node edge : node.edges) {
+			if (!inRecursion[edge.row][edge.coloumn]) {
+				inRecursion[edge.row][edge.coloumn] = true; //Infinite recursion not allowed!.
+				connectNode(edge); // Check which nodes it can reach!
+				//Because of the structure, it is a depth first search.
+			}
+		}
+
 	}
-	
+
 	private List<Node> getReachableNodes(Node startingNode) {
-		ArrayList<Node> listOfNodes = new ArrayList<Node>();	
-		getRunningReachableNodes(startingNode, listOfNodes);
-		getBadJumpReachableNodes(startingNode, listOfNodes);	
+		ArrayList<Node> listOfNodes = new ArrayList<Node>();
+		//Three different ways to find the reachable nodes from a given position:
+		//getRunningReachableNodes(startingNode, listOfNodes); //TODO Obs. no need to return a list of nodes
+		//getBadJumpReachableNodes(startingNode, listOfNodes);
 		getPolynomialReachableNodes(startingNode, listOfNodes);
 		return listOfNodes;
 	}
-	
-	private List<Node> getBadJumpReachableNodes(Node startingNode, List<Node> listOfNodes){
-		//(bad) Jumping and falling.
-		for (short i = -5; i < MARIO_JUMP_LENGHT ; i++) { //Starting at 5 blocks below, to simulate falling.
-			if(startingNode.row + i >= GRID_SIZE || startingNode.row + i < 0 ||
-			   startingNode.row - i >= GRID_SIZE || startingNode.row - i < 0) continue;
+
+	private List<Node> getBadJumpReachableNodes(Node startingNode, List<Node> listOfNodes) {
+		// (bad) Jumping and falling.
+		for (short i = -5; i < MARIO_JUMP_LENGHT; i++) { // Starting at 5 blocks
+															// below, to
+															// simulate falling.
+			if (startingNode.row + i >= GRID_SIZE || startingNode.row + i < 0 || startingNode.row - i >= GRID_SIZE
+					|| startingNode.row - i < 0)
+				continue;
 			if (startingNode.coloumn + 1 < GRID_SIZE) {
 				if (observationGraph[startingNode.row - i][startingNode.coloumn + 1] == null) {
-					observationGraph[startingNode.row - i][startingNode.coloumn + 1] = new Node((short) (startingNode.x + 1), startingNode.y, (short) (startingNode.coloumn + 1),
-																									 (short) (startingNode.row - i), currentObservation[startingNode.row - i][startingNode.coloumn + 1]);
+					observationGraph[startingNode.row - i][startingNode.coloumn + 1] = new Node(
+							(short) (startingNode.x + 1), startingNode.y, (short) (startingNode.coloumn + 1),
+							(short) (startingNode.row - i),
+							currentObservation[startingNode.row - i][startingNode.coloumn + 1]);
 				}
-				listOfNodes.add(observationGraph[startingNode.row - i][startingNode.coloumn + 1]);				
-			}			
-			//Minus
-			if(startingNode.coloumn - 1 >= 0) {
+				listOfNodes.add(observationGraph[startingNode.row - i][startingNode.coloumn + 1]);
+			}
+			// Minus
+			if (startingNode.coloumn - 1 >= 0) {
 				if (observationGraph[startingNode.row - i][startingNode.coloumn - 1] == null) {
-					observationGraph[startingNode.row - i][startingNode.coloumn - 1] = new Node((short) (startingNode.x + 1), startingNode.y, (short) (startingNode.coloumn - 1),
-							 																			(short) (startingNode.row - i), currentObservation[startingNode.row - i][startingNode.coloumn - 1]);
+					observationGraph[startingNode.row - i][startingNode.coloumn - 1] = new Node(
+							(short) (startingNode.x + 1), startingNode.y, (short) (startingNode.coloumn - 1),
+							(short) (startingNode.row - i),
+							currentObservation[startingNode.row - i][startingNode.coloumn - 1]);
 				}
-				listOfNodes.add(observationGraph[startingNode.row - i][startingNode.coloumn - 1]);				
+				listOfNodes.add(observationGraph[startingNode.row - i][startingNode.coloumn - 1]);
 			}
 		}
 		return listOfNodes;
 	}
-	
-	private List<Node> getRunningReachableNodes(Node startingNode, List<Node> listOfNodes){
-		//(bad) Running:
-		for(short i = 1; i<= 1; i++) {
-			//Plus
+
+	private List<Node> getRunningReachableNodes(Node startingNode, List<Node> listOfNodes) {
+		// (bad) Running:
+		for (short i = 1; i <= 1; i++) {
+			// Plus
 			if (startingNode.coloumn + 1 < GRID_SIZE) {
 				if (observationGraph[startingNode.row][startingNode.coloumn + i] == null) {
-					observationGraph[startingNode.row][startingNode.coloumn + i] = new Node((short) (startingNode.x + i), startingNode.y, (short) (startingNode.coloumn + i),
-																									 startingNode.row, currentObservation[startingNode.row][startingNode.coloumn + i]);
+					observationGraph[startingNode.row][startingNode.coloumn + i] = new Node(
+							(short) (startingNode.x + i), startingNode.y, (short) (startingNode.coloumn + i),
+							startingNode.row, currentObservation[startingNode.row][startingNode.coloumn + i]);
 				}
-				listOfNodes.add(observationGraph[startingNode.row][startingNode.coloumn + 1]);				
-			}			
-			//Minus
-			if(startingNode.coloumn - 1 >= 0) {
+				listOfNodes.add(observationGraph[startingNode.row][startingNode.coloumn + 1]);
+			}
+			// Minus
+			if (startingNode.coloumn - 1 >= 0) {
 				if (observationGraph[startingNode.row][startingNode.coloumn - i] == null) {
-					observationGraph[startingNode.row][startingNode.coloumn - i] = new Node((short) (startingNode.x + i), startingNode.y, (short) (startingNode.coloumn - i),
-							 																			startingNode.row, currentObservation[startingNode.row][startingNode.coloumn - i]);
+					observationGraph[startingNode.row][startingNode.coloumn - i] = new Node(
+							(short) (startingNode.x + i), startingNode.y, (short) (startingNode.coloumn - i),
+							startingNode.row, currentObservation[startingNode.row][startingNode.coloumn - i]);
 				}
-				listOfNodes.add(observationGraph[startingNode.row][startingNode.coloumn - 1]);				
+				listOfNodes.add(observationGraph[startingNode.row][startingNode.coloumn - 1]);
 			}
 		}
 		return listOfNodes;
 	}
+
+	/*** Finds the possible places that mario can jump to, from the given position, 
+	 *   and adds the to the given list of nodes. This is done by simulating the jump as a polynomial,
+	 *   and finding the place it collides with something.
+	 * 
+	 * @return
+	 */
+	private List<Node> getPolynomialReachableNodes(Node startingNode, List<Node> listOfNodes) {
+		//TODO Extra ting der kan tilføjes: polynomium hop til fjender!
+		
+		// Starting of with just one polynomial:
+		//Calculates the different values used by the polynomial:
+		float jumpRange; //TODO Fix problemmet med 5.99 i stedet for 6, with jumpRange
+		SecondOrderPolynomial polynomial = new SecondOrderPolynomial(); //The jump polynomial.
+		for (jumpRange = MAX_JUMP_RANGE; jumpRange <= MAX_JUMP_RANGE; jumpRange++) {
+			polynomial.setToJumpPolynomial(startingNode, jumpRange, JUMP_HEIGHT);
+			//Starts of from Mario's initial position:
+			short currentXPosition = startingNode.coloumn;
+			float formerYPosition = startingNode.row;
+			short formerLowerYPosition = startingNode.row;
+			boolean hasMetHardGround = false;		
+			boolean isPastTopPunkt = false;
+					
+			while (!hasMetHardGround &&
+					currentXPosition + 1 < GRID_SIZE &&
+					currentXPosition > 0) { //Doesen't take falling down into a hole into account.
+				currentXPosition++;
+				float currentYPosition = polynomial.f(currentXPosition);
+				short currentLowerYPosition = (short) currentYPosition; //Automatic flooring included!
+				// TODO change to take the sign into account
+				short bound = (short) (startingNode.row - (currentLowerYPosition-startingNode.row));
+				
+				for (short y = formerLowerYPosition; (currentXPosition <= polynomial.getCeiledTopPunktX())? y >= bound : y<= bound; y = (short) ((currentXPosition <= polynomial.getCeiledTopPunktX())? y - 1: y + 1)) { //Can be split up into two for loops for added efficieny.
+					if (isSolid(currentObservation[y][currentXPosition])) {
+						hasMetHardGround = true;
+						Node fallDownPosition = getFallDownPosition(y, currentXPosition);
+						if(fallDownPosition != null) {
+							if (observationGraph[fallDownPosition.row][fallDownPosition.coloumn] != null) {
+								observationGraph[fallDownPosition.row][fallDownPosition.coloumn] = fallDownPosition;
+							}
+							
+							listOfNodes.add(fallDownPosition);
+						}
+						break;
+					} else if (canMarioStandThere(currentXPosition, y) && (currentXPosition >= polynomial.getCeiledTopPunktX())) { 
+						//Checks if mario can stand on the current block, and if he is currently in the falling part of the polynomial.
+						//This is not done by precisely checking if it hits the ground, for a reason --> Mario's can move while jumping.
+						//TODO Later it would be more appropriate to use a isFalling boolean, f.eks. hvis mario glider langs en mur,
+						//hvormed x positionen ikke ændrer sig.
+						
+						if (observationGraph[y][currentXPosition] == null) {
+							observationGraph[y][currentXPosition] = new Node(currentXPosition, y, currentXPosition, y,currentObservation[currentXPosition][y]);
+						}					
+						listOfNodes.add(observationGraph[y][currentXPosition]);
+					}		
+				}
+				
+				formerYPosition = currentYPosition;
+				formerLowerYPosition = bound;
+			}			
+		}
+		return listOfNodes; //No guarantee that there are no duplicate nodes.
+	}
+
 	
-	private List<Node> getPolynomialReachableNodes(Node startingNode, List<Node> listOfNodes){		
-		
-		
-		
-		
-		
-		return listOfNodes;
+	private Node getFallDownPosition(short row, short coloumn) {
+		// Not including zero, as there needs to be a block below mario to fall
+		// down and stand on.
+		for (short yPosition = row; yPosition < GRID_SIZE - 1; yPosition++) {
+			if (canMarioStandThere(coloumn,yPosition))
+				return new Node(coloumn, row, coloumn, yPosition, currentObservation[yPosition][coloumn]);
+		}
+
+		return null;
 	}
 	
+	private short newYPosition(short x, short topPunktX,short currentY) {
+		if(x < topPunktX) return currentY--;
+		else return currentY++;
+	}
+
 	private boolean canMarioStandThere(Node place) {
-		return  isOnSolidGround(place) && !isSolid(place.type);
+		return canMarioStandThere(place.coloumn,place.row);
 	}
-	
-	private boolean isOnSolidGround(Node place) {
-		return isSolid(currentObservation[place.row+1][place.coloumn]);
+
+	private boolean canMarioStandThere(short coloumn,short row) {
+		return isOnSolidGround(row, coloumn) && !isSolid(currentObservation[row][coloumn]);
 	}
-	
-	private boolean isSolid(byte blockType){
-		return blockType != 0;//TODO(*) Fix
+
+	private boolean isOnSolidGround(short row, short coloumn) {
+		return isSolid(currentObservation[row + 1][coloumn]) || currentObservation[row + 1][coloumn] == -11; //TODO Fix in general.
 	}
-	
-	
-	
+
+	private boolean isSolid(byte blockType) {
+		return blockType != 0 && blockType != -11;// TODO(*) Fix
+	}
 	
 }
---
+++ src/MarioAI/SecondOrderPolynomial.java
@@ -0,0 +1,58 @@
+package MarioAI;
+
+public class SecondOrderPolynomial{
+	private float a,b,c; //Parameters of the polynomial.
+	private float topPunktX, topPunktY; //Cordinates of the toppunkt
+	//TODO Maybe change ceil after discussions. Problem associated with making this into an integer
+	//, as sometimes two possible jump position in a row will be registrated as possible to reach,
+	//even though it is only one it should be possible to reach. Mario changing position in the air can compensate for this.
+	private short ceiledTopPunktX,ceiledTopPunktY; //Ceildes coordinates of the toppunkt
+	
+	public SecondOrderPolynomial(Node startingPosition, float jumpRange, float jumpHeight) {
+		setToJumpPolynomial(startingPosition, jumpRange, jumpHeight);
+	}
+	
+	public SecondOrderPolynomial() {
+		a = 0;
+		b = 0;
+		c = 0;
+	}
+	
+	//
+	public void setToJumpPolynomial(Node startingPosition, float jumpRange, float jumpHeight) {
+		a = -4 * jumpHeight / (jumpRange * jumpRange);
+		b = (8 * startingPosition.coloumn + 4 * jumpRange) * jumpHeight / (jumpRange * jumpRange);
+		c = (-4 * startingPosition.coloumn  * (startingPosition.coloumn + jumpRange) * jumpHeight + startingPosition.row
+				* jumpRange * jumpRange) / (jumpRange * jumpRange);
+		setTopPunkt();
+	}
+	
+
+	public float getTopPunktX() {
+		return (topPunktX);
+	}
+	
+	public float getToppunktY() {
+		return topPunktY;
+	}
+
+	public float getCeiledTopPunktX() {
+		return (ceiledTopPunktX);
+	}
+	
+	public float getCeiledTopPunktY() {
+		return ceiledTopPunktY;
+	}
+	
+	private void setTopPunkt() {
+		topPunktX = ((-b/a)/2);
+		topPunktY = f(topPunktX);
+		ceiledTopPunktX = (short) Math.ceil(ceiledTopPunktX);
+		ceiledTopPunktY = (short) Math.ceil(ceiledTopPunktY);
+	}
+		
+	public float f(float x) {
+		return a*x*x+b*x+c;
+	}
+	
+}
