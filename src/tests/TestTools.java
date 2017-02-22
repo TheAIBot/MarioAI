d95ba7b2b3eaebbd3fac9bad44324915b6df3a53
TheAIBot <andreasuranus@hotmail.com>
2017-02-22 11:18:43 +0100

    made the level simulate the fist 30 ticks after loading to init it

--- src/tests/TestTools.java
+++ src/tests/TestTools.java
@@ -27,18 +27,18 @@ import ch.idsia.tools.ToolsConfigurator;
 
 public class TestTools {
 
-	public static void runOneTick(Environment observation)
-	{
-		((MarioComponent)observation).runOneTick();
+	private static final int LEVEL_INIT_TICKS = 30;
+
+	public static void runOneTick(Environment observation) {
+		((MarioComponent) observation).runOneTick();
 	}
-	
-	public static void runWholeLevel(Environment observation)
-	{
-		while(((MarioComponent)observation).runOneTick() == Mario.STATUS_RUNNING);
+
+	public static void runWholeLevel(Environment observation) {
+		while (((MarioComponent) observation).runOneTick() == Mario.STATUS_RUNNING)
+			;
 	}
-	
-	public static Environment loadLevel(String filepath)
-	{
+
+	public static Environment loadLevel(String filepath) {
 		Level level = null;
 		try {
 			level = Level.load(new DataInputStream(new FileInputStream(filepath)));
@@ -46,21 +46,29 @@ public class TestTools {
 			Assert.fail(e.getMessage());
 			return null;
 		}
-		
-        Agent controller = new FastAndFurious();
-        EvaluationOptions options = new CmdLineOptions(new String[0]);
-        options.setAgent(controller);
-        Task task = new ProgressTask(options);
-        options.setMaxFPS(false);
-        options.setVisualization(true);
-        options.setNumberOfTrials(1);
-        options.setMatlabFileName("");
-        options.setLevelRandSeed(422);
-        //options.setLevelRandSeed((int) (Math.random () * Integer.MAX_VALUE));
-        options.setLevelDifficulty(-1);
-        task.setOptions(options);
 
-        return (Environment)task.loadLevel(level, controller);
+		Agent controller = new FastAndFurious();
+		EvaluationOptions options = new CmdLineOptions(new String[0]);
+		options.setAgent(controller);
+		Task task = new ProgressTask(options);
+		options.setMaxFPS(false);
+		options.setVisualization(true);
+		options.setNumberOfTrials(1);
+		options.setMatlabFileName("");
+		options.setLevelRandSeed(422);
+		// options.setLevelRandSeed((int) (Math.random () * Integer.MAX_VALUE));
+		options.setLevelDifficulty(-1);
+		task.setOptions(options);
+
+		Environment environment = (Environment) task.loadLevel(level, controller);
+		waitForLevelInit(environment);
+		return environment;
 	}
-	
+
+	private static void waitForLevelInit(Environment observation) {
+		for (int i = 0; i < LEVEL_INIT_TICKS; i++) {
+			runOneTick(observation);
+		}
+	}
+
 }
