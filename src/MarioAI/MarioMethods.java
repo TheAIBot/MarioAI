8a687d95fdf5e226b5f6b3f274608354866b37c7
TheAIBot <andreasuranus@hotmail.com>
2017-02-22 23:35:43 +0100

    fixed precision error when retrieving marios position

--- src/MarioAI/MarioMethods.java
+++ src/MarioAI/MarioMethods.java
@@ -6,21 +6,21 @@ public class MarioMethods {
 	
 	public static int getMarioXPos(final float[] marioPos)
 	{
-		return (int)Math.round(marioPos[0]) / BLOCK_PIXEL_SIZE;
+		return (int)(marioPos[0] / BLOCK_PIXEL_SIZE);
 	}
 	
 	public static int getMarioYPos(final float[] marioPos)
 	{
-		return (int)Math.round(marioPos[1]) / BLOCK_PIXEL_SIZE;
+		return (int)(marioPos[1] / BLOCK_PIXEL_SIZE);
 	}
 	
 	public static float getPreciseMarioXPos(final float[] marioPos)
 	{
-		return Math.round(marioPos[0]) / BLOCK_PIXEL_SIZE;
+		return marioPos[0] / BLOCK_PIXEL_SIZE;
 	}
 	
 	public static float getPreciseMarioYPos(final float[] marioPos)
 	{
-		return Math.round(marioPos[1]) / BLOCK_PIXEL_SIZE;
+		return marioPos[1] / BLOCK_PIXEL_SIZE;
 	}
 }
