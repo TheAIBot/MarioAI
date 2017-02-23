d68974e3760d7a09b893ffd56565540f2593fc3a
TheAIBot <andreasuranus@hotmail.com>
2017-02-24 00:39:40 +0100

    changed MarioControls to handle variable jumo height

--- src/MarioAI/MarioMethods.java
+++ src/MarioAI/MarioMethods.java
@@ -3,15 +3,16 @@ package MarioAI;
 public class MarioMethods {
 	
 	private static final int BLOCK_PIXEL_SIZE = 16;
+	private static final float BLOCK_CENTERING_OFFSET = 0.5f;
 	
 	public static int getMarioXPos(final float[] marioPos)
 	{
-		return (int)(marioPos[0] / BLOCK_PIXEL_SIZE);
+		return (int)getPreciseMarioXPos(marioPos);
 	}
 	
 	public static int getMarioYPos(final float[] marioPos)
 	{
-		return (int)(marioPos[1] / BLOCK_PIXEL_SIZE);
+		return (int)getPreciseMarioYPos(marioPos);
 	}
 	
 	public static float getPreciseMarioXPos(final float[] marioPos)
@@ -22,5 +23,15 @@ public class MarioMethods {
 	public static float getPreciseMarioYPos(final float[] marioPos)
 	{
 		return marioPos[1] / BLOCK_PIXEL_SIZE;
+	}
+	
+	public static float getPreciseCenteredMarioXPos(final float[] marioPos)
+	{
+		return getPreciseMarioXPos(marioPos) - BLOCK_CENTERING_OFFSET;
+	}
+	
+	public static float getPreciseCenteredMarioYPos(final float[] marioPos)
+	{
+		return getPreciseMarioYPos(marioPos) - BLOCK_CENTERING_OFFSET;
 	}
 }
