97a9d5e4113e97bce479c33b157d4b2732fdf8c6
TheAIBot <andreasuranus@hotmail.com>
2017-03-01 12:51:18 +0100

    fixed fast simulation

--- src/tests/TestTools.java
+++ src/tests/TestTools.java
@@ -28,7 +28,8 @@ public class TestTools {
 	}
 
 	public static void runWholeLevel(Environment observation) {
-		while (((MarioComponent) observation).runOneTick() == Mario.STATUS_RUNNING) { }
+		((MarioComponent) observation).run1(0, 1);
+		//while (((MarioComponent) observation).runOneTick() == Mario.STATUS_RUNNING) { }
 	}
 	
 	public static byte[][] getLevelMap(Environment observation)
@@ -52,7 +53,7 @@ public class TestTools {
 		options.setVisualization(true);
 		options.setNumberOfTrials(1);
 		options.setMatlabFileName("");
-		options.setLevelRandSeed(422);
+		options.setLevelRandSeed(42);
 		// options.setLevelRandSeed((int) (Math.random () * Integer.MAX_VALUE));
 		options.setLevelDifficulty(-1);
 		task.setOptions(options);
