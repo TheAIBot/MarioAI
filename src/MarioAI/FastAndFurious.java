840598b21bdaeafa9075725cd2be1bdd86c229cf
Dragerytteren <fantasterix@gmail.com>
2017-03-08 11:07:53 +0100

    Merge error revert

--- src/MarioAI/FastAndFurious.java
+++ src/MarioAI/FastAndFurious.java
@@ -54,10 +54,7 @@ public class FastAndFurious implements Agent {
 				DebugDraw.resetGraphics(observation);
 				DebugDraw.drawBlockBeneathMarioNeighbors(observation, graph);
 				DebugDraw.drawNeighborPaths(observation, graph);
-<<<<<<< HEAD
-				DebugDraw.drawReachableNodes(observation, graph);
-=======
->>>>>>> parent of 32d95b0... Added debug reachable nodes functionality
+				//DebugDraw.drawReachableNodes(observation, graph);
 				DebugDraw.drawPathOptionNodes(observation, graph);
 			}
 		}
@@ -70,15 +67,12 @@ public class FastAndFurious implements Agent {
 				}
 			}
 			if (DEBUG) DebugDraw.drawPath(observation, newestPath);
-<<<<<<< HEAD
 		} else if (tickCount > 30){
 			System.out.println("Fail");
 			Grapher.graph(graph.getLevelMatrix(), graph.getMarioNode(observation));
 			List<DirectedEdge> path = AStar.runMultiNodeAStar(graph.getMarioNode(observation), graph.getGoalNodes());
 			newestPath = path;
 			System.out.println("");
-=======
->>>>>>> parent of 32d95b0... Added debug reachable nodes functionality
 		}
 		tickCount++;
 		graph.printMatrix(observation);
