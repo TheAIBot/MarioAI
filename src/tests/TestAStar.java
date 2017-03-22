2e32a5ac9df65bd11d37458c21bf3f3408cdb212
Dragerytteren <fantasterix@gmail.com>
2017-03-22 11:47:19 +0100

    Change from assertTrue to assertEquals in TestAStar

--- src/tests/TestAStar.java
+++ src/tests/TestAStar.java
@@ -1,11 +1,9 @@
 package tests;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
 
 import MarioAI.AStar;
 import MarioAI.FastAndFurious;
@@ -14,7 +12,6 @@ import MarioAI.SecondOrderPolynomial;
 import MarioAI.graph.DirectedEdge;
 import MarioAI.graph.Graph;
 import MarioAI.graph.Grapher;
-import MarioAI.graph.Node;
 import ch.idsia.ai.agents.Agent;
 import ch.idsia.mario.environments.Environment;
 
@@ -22,10 +19,11 @@ public class TestAStar {
 	Agent agent;
 	Environment observation;
 	Graph graph;
+	final float delta = 0.05;
 	
 	public void setUp(String levelName) {
 		agent = new FastAndFurious();
-		observation = TestTools.loadLevel("src/tests/testLevels/" + levelName + ".lvl", agent);
+		observation = TestTools.loadLevel("" + levelName + ".lvl", agent);
 		
 		TestTools.runOneTick(observation);
 		graph = new Graph();
@@ -45,8 +43,8 @@ public class TestAStar {
 		
 		float c = 1.0f;
 		for (DirectedEdge directedEdge : path) {
-			assertTrue(directedEdge.target.gScore == c);
-			assertTrue(directedEdge.target.fScore == 1000 - c);
+			assertEquals(directedEdge.target.gScore, c, delta);
+			assertEquals(directedEdge.target.fScore == 1000 - c, delta);
 			assertTrue(directedEdge instanceof Running);
 //			try {
 //				Running test = (Running) directedEdge;
@@ -56,7 +54,7 @@ public class TestAStar {
 			c++;
 		}
 		// goal node has been removed from path returned
-		assertTrue(path.get(path.size()-1).target.x != 1000);
+		assertNotEquals(path.get(path.size()-1).target.x, 1000, delta);
 	}
 	
 	/**
@@ -64,7 +62,7 @@ public class TestAStar {
 	 */
 	@Test
 	public void testAStarJumping() {
-		setUp("testJumping");
+		setUp("platformJump");
 		
 		List<DirectedEdge> path = AStar.runMultiNodeAStar(graph.getMarioNode(observation), graph.getGoalNodes());
 		assertTrue(path != null);
@@ -72,9 +70,9 @@ public class TestAStar {
 		DirectedEdge e1 = path.get(0);
 		DirectedEdge e2 = path.get(1);
 		DirectedEdge eN = path.get(5);
-		assertTrue(e1.target.gScore == e1.target.y - e1.source.y);
-		assertTrue(e1.target.gScore == e2.target.y - e2.source.y);
-		assertTrue(e1  instanceof SecondOrderPolynomial);
+		assertEquals(e1.target.gScore, e1.target.y - e1.source.y, delta);
+		assertEquals(e1.target.gScore, e2.target.y - e2.source.y, delta);
+		assertTrue(e1 instanceof SecondOrderPolynomial);
 		assertTrue(e2 instanceof SecondOrderPolynomial);
 		assertTrue(eN instanceof Running);		
 	}
