e1bc7f36e1bf7634d8969477f622d5bb1ab3006d
TheAIBot <andreasuranus@hotmail.com>
2017-04-21 16:45:52 +0200

    moved abstract method declarations up top in directededge

--- src/MarioAI/graph/edges/DirectedEdge.java
+++ src/MarioAI/graph/edges/DirectedEdge.java
@@ -14,11 +14,12 @@ public abstract class DirectedEdge {
 		this.target = target;
 	}
 	
-	@Override
-	public int hashCode() {
-		return hash;
-	}
+	public abstract float getMaxY();
 	
+	public abstract float getWeight();
+	
+	protected abstract int getExtraEdgeHashcode();
+		
 	@Override
 	public boolean equals(Object b) {
 		if (b == null) {
@@ -31,11 +32,7 @@ public abstract class DirectedEdge {
 			return false;
 		}
 	}
-	
-	public abstract float getMaxY();
-	
-	public abstract float getWeight();
-	
+		
 	public void setMoveInfo(MovementInformation moveInfo) {
 		this.moveInfo = moveInfo;
 	}
@@ -45,14 +42,12 @@ public abstract class DirectedEdge {
 	}
 	
 	@Override
+	public int hashCode() {
+		return hash;
+	}
+	
+	@Override
 	public String toString() {
 		return "[" + source.x + " : " + source.y + "]" + " --> " + "[" + target.x + " : " + target.y + "]" + " H: " + Math.round(getMaxY());
 	}
-		
-	public boolean hasReachedTarget() {
-		return true; //TODO make.
-	}
-	
-	protected abstract int getExtraEdgeHashcode();
-	
 }
