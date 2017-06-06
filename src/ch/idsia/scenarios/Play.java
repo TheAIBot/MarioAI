143e22f3be515267fa2b7a20ef1032417a41d32e
Dragerytteren <fantasterix@gmail.com>
2017-06-06 13:03:24 +0200

    edit play

--- src/ch/idsia/scenarios/Play.java
+++ src/ch/idsia/scenarios/Play.java
@@ -1,6 +1,7 @@
 package ch.idsia.scenarios;
 
 import ch.idsia.ai.agents.Agent;
+import ch.idsia.ai.agents.AgentsPool;
 import ch.idsia.ai.tasks.ProgressTask;
 import ch.idsia.ai.tasks.Task;
 import ch.idsia.tools.CmdLineOptions;
@@ -12,6 +13,10 @@ public class Play {
 	public static void main(String[] args) {
         Agent controller = new TickBasedAgent();
         //HumanKeyboardAgent controller = new HumanKeyboardAgent();
+        if (args.length > 0) {
+            controller = AgentsPool.load (args[0]);
+            AgentsPool.addAgent(controller);
+        }
         EvaluationOptions options = new CmdLineOptions(new String[0]);
         options.setAgent(controller);
         Task task = new ProgressTask(options);
@@ -19,9 +24,10 @@ public class Play {
         options.setVisualization(true);
         options.setNumberOfTrials(1);
         options.setMatlabFileName("");
-        int seed = (int) (Math.random() * Integer.MAX_VALUE);
-        options.setLevelRandSeed(seed);
-        System.out.println("Seed = " + seed);
-        options.setLevelDifficulty(0);
+        options.setLevelRandSeed(1);//(int) (Math.random () * Integer.MAX_VALUE));
+        options.setLevelDifficulty(-1);
+        task.setOptions(options);
+        
+        System.out.println ("Score: " + task.evaluate (controller)[0]);
 	}
 }
