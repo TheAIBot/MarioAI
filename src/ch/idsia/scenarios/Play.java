98dd7f07b22aa1aeb0e03b49c0e5670ac34bd27b
Lombre <jesper.birch3@gmail.com>
2017-06-16 10:53:21 +0200

    Unlimited comment works

--- src/ch/idsia/scenarios/Play.java
+++ src/ch/idsia/scenarios/Play.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import MarioAI.FastAndFurious;
+import ch.idsia.ai.agents.human.HumanKeyboardAgent;
 import ch.idsia.ai.tasks.ProgressTask;
 import ch.idsia.ai.tasks.Task;
 import ch.idsia.mario.environments.Environment;
@@ -47,54 +48,55 @@ public class Play {
 			}
 			TestTools.runWholeLevel(observation);
 		} else {
-			FastAndFurious controller = new FastAndFurious();
-	        //HumanKeyboardAgent controller = new HumanKeyboardAgent();
-	        EvaluationOptions options = new CmdLineOptions(new String[0]);
-	        options.setAgent(controller);
-	        Task task = new ProgressTask(options);
-	        options.setMaxFPS(false);
-	        options.setVisualization(true);
-	        options.setNumberOfTrials(1);
-	        options.setMatlabFileName("");
-	        int seed = (int) (Math.random() * Integer.MAX_VALUE);
-	        options.setLevelRandSeed(seed);
+			//FastAndFurious controller = new FastAndFurious();
+			HumanKeyboardAgent controller = new HumanKeyboardAgent();
+			EvaluationOptions options = new CmdLineOptions(new String[0]);
+			options.setAgent(controller);
+			Task task = new ProgressTask(options);
+			options.setMaxFPS(false);
+			options.setVisualization(true);
+			options.setNumberOfTrials(1);
+			options.setMatlabFileName("");
+			int seed = (int) (Math.random() * Integer.MAX_VALUE);
+			options.setLevelRandSeed(seed);
             
-	        if (new File(FastAndFurious.saveStateFileName).exists()) {
-	        	String fileContent = null;
+			if (new File(FastAndFurious.saveStateFileName).exists()) {
+				String fileContent = null;
 				try {
 					fileContent = Files.readAllLines(Paths.get(FastAndFurious.saveStateFileName)).get(0);
 					
 					final long loadedSeed = Long.parseLong(fileContent.split(" ")[0]);
 					final int tick = Integer.parseInt(fileContent.split(" ")[1]);
 					
-					controller.runToTick(tick);
+					//controller.runToTick(tick);
 					options.setLevelRandSeed((int)loadedSeed);
 					options.setMaxFPS(true);
 				} catch (IOException e) {
 					System.out.println("Failed to load level state");
 				}
 			}
-            
-	        System.out.println("Seed = " + seed);
-	        options.setLevelRandSeed(1045681246);
-	        //options.setLevelRandSeed(1610614020); //Collision errors, falls of ledge.
-	        //options.setLevelRandSeed(264076394);
-	        //options.setLevelRandSeed(933697569); // the best seed ever, difficulty 40
-	        //options.setLevelRandSeed(324150513); //the best seed ever 2
-	        //options.setLevelRandSeed(898452612); //Difficulty 1
-	        //options.setLevelRandSeed(632962519); //Difficulty 1, good seed
-	        //options.setLevelRandSeed(860788790);
-	        //options.setLevelRandSeed(1145934057);
-	        //options.setLevelRandSeed(1319952038); //Difficulty 2, error, especially with reusing speed nodes.
-	        //options.setLevelRandSeed(1585046168);
-	        //options.setLevelRandSeed(3261372);
-	        //options.setLevelRandSeed(41);
-	        //options.setLevelRandSeed(42);
-	        //options.setLevelRandSeed(650);
-	        //options.setLevelRandSeed(666);
-	        //options.setLevelRandSeed(42243);
-	        //options.setLevelRandSeed(1028660435);
-	        //options.setLevelRandSeed(1905810938); //Bug here without running edges
+			    
+			System.out.println("Seed = " + seed);
+			//options.setLevelRandSeed(124613827);
+			//options.setLevelRandSeed(1045681246);
+			//options.setLevelRandSeed(1610614020); //Collision errors, falls of ledge.
+			//options.setLevelRandSeed(264076394);
+			//options.setLevelRandSeed(933697569); // the best seed ever, difficulty 40
+			//options.setLevelRandSeed(324150513); //the best seed ever 2
+			//options.setLevelRandSeed(898452612); //Difficulty 1
+			//options.setLevelRandSeed(632962519); //Difficulty 1, good seed
+			//options.setLevelRandSeed(860788790);
+			//options.setLevelRandSeed(1145934057);
+			//options.setLevelRandSeed(1319952038); //Difficulty 2, error, especially with reusing speed nodes.
+			//options.setLevelRandSeed(1585046168);
+			//options.setLevelRandSeed(3261372);
+			//options.setLevelRandSeed(41);
+			//options.setLevelRandSeed(42);
+			//options.setLevelRandSeed(650);
+			//options.setLevelRandSeed(666);
+			//options.setLevelRandSeed(42243);
+			//options.setLevelRandSeed(1028660435);
+			//options.setLevelRandSeed(1905810938); //Bug here without running edges
 
 	        /*
 860788790 //Error at difficulty 2
@@ -114,7 +116,7 @@ public class Play {
 	        
 	        //options.setLevelRandSeed(42243);(*) Includes a missing feature.
 	        //options.setLevelDifficulty(2);
-	        options.setLevelDifficulty(-1);
+	        options.setLevelDifficulty(2);
 	        task.setOptions(options);
 	        
 	        System.out.println ("Score: " + task.evaluate (controller)[0]);
